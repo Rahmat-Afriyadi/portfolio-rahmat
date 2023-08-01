57962f73c4cb1940ddab248d4d9dace34f8594c5
Rahmat-Afriyadi <rahmatcv725@gmail.com>
2023-08-01 16:05:24 +0700

    fix projects

--- pages/combine.js
+++ pages/combine.js
@@ -3,7 +3,7 @@ import dynamic from "next/dynamic";
 const Hero = dynamic(() => import("../components/Hero"), { ssr: false });
 const Profile = dynamic(() => import("./profile"), { ssr: false });
 
-import Data from "./data/projects.json";
+import Data from "../components/data/projects.json";
 
 export default function Home({ props }) {
   const data = props;
